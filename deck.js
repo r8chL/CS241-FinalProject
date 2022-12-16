c847e714a35172d57057ed9ab5aad2edee8cd007
aidanmm4 <113111785+aidanmm4@users.noreply.github.com>
2022-12-16 14:17:50 -0500

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,4 +1,4 @@
-const SUITS = ["♥", "♦", "♠", "♣"]
+const SUITS = ["♣", "♠", "♦", "♥"] //static variable for card suits
 const VALUES = [
     "A",
     "2",
@@ -13,21 +13,63 @@ const VALUES = [
     "J",
     "Q",
     "K"
-  ]
+] //static variable for card values
 
-class Deck{//needs 2 b flexible enough for a full deck of cards and someone's hand in cards
-    constructor(cards){
-        this.cards = []//is an empty array
+
+
+export default class Deck{
+    constructor (cards = freshDeck() ) { 
+         this.cards = cards 
+    }
+
+    get numberOfCards() { //creates a getter for the length of the cards array
+        return this.cards.length
+    }
+
+    topCard () {
+        return this.cards.shift() //shift takes from the top 
+    }
+
+    puttoBottom (card) {
+        this.cards.push(card)
+    }
+
+    shuffle () {
+        for (let i = this.numberOfCards - 1; i > 0; i --) { //starts at the end of the deck and flips it with a card that comes earlier in the deck 
+            const newIndex = Math.floor(Math.random() * (i + 1)) //will give us an index that hasn't been accessed yet, and by using Math.floor() makes sure it is an integer value because Math.random() selects any number between 1 and 0
+            const currentValue = this.cards[newIndex] //swaps the card we're currently on with new card 
+            this.cards[newIndex] = this.cards[i] //current value acts as a temp value, and then switches card we just got with the card at current-index i
+            this.cards[i] = currentValue //makes the final swap
+        }
     }
 }
 
-class Card{
-    constructor(suit, value){
-        this.suit = suit
-        this.value = value
+
+class Card {
+    constructor(suit, value) {
+         this.suit = suit
+         this.value = value 
+    }
+
+    get color () {
+        return this.suit === "♣" || this.suit === "♠" ? 'black' : "red"
+    }
+
+    getHTML () {
+        const cardDiv = document.createElement('div')
+        cardDiv.innerText = this.suit
+        cardDiv.classList.add("card", this.color)
+        cardDiv.dataset.value = `${this.value} ${this.suit}` //backtick it 
+        return cardDiv
     }
 }
 
-function freshDeck(){//makes 
 
+
+function freshDeck() {
+    return SUITS.flatMap(suit => { //by using flatmap, we will get a single array with 52 cards instead of an array with 4 separate arrays with 13 cards. 
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
 }
